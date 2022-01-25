f414eec32d482222d9a55821ea225057ee69df63
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2022-01-25 17:01:15 +0100

    changes

--- r_charts.R
+++ r_charts.R
@@ -2,24 +2,20 @@
 #Charts
 ##Boxplot
 help("boxplot")
-### By default
+### By data
 boxplot(iris)
 ### By formula
-boxplot(iris, range = 1.5, width = NULL, varwidth = FALSE,
-        notch = FALSE, outline = TRUE, names, plot = TRUE,
-        border = par("fg"), col = "lightgray", log = "",
-        pars = list(boxwex = 1.5, staplewex = 0.5, outwex = 0.5), 
-        horizontal = TRUE, add = TRUE, at = NULL)
+boxplot(Sepal.Length ~ Petal.Length,iris)
+
+### Quartile --> Use range
+boxplot(iris, range = 1)
 
 ##Plot
 help("plot")
+### By data
 plot(iris)
+### By formula
 plot(Sepal.Length ~ Petal.Length, data=iris)
-plot(iris, y = NULL,  xlim = NULL, ylim = NULL,
-     log = "", main = NULL, sub = NULL,
-     ann = par("ann"),
-     panel.first = NULL, panel.last = NULL, asp = NA,
-     xgap.axis = NA, ygap.axis = NA)
 
 
 ##Barplot
